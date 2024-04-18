e6d9081d80af2b8c41886edf245c2d809c8b2d3b
davidalifff <mochnalif3@gmail.com>
2024-04-18 18:05:17 +0700

    Initial + Login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    } else {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+    }
+
+    public function logout(){
+        auth()->logout();
+        return response()->json('', 200);
+    }
+}
